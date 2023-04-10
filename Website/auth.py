13227ce75ba70b9288cece43f8d0b847ec212738
Lucas2 <contatolucasoficial0@gmail.com>
2023-04-10 15:23:08 -0300

    Commit 5 - uso de templates

--- Website/auth.py
+++ Website/auth.py
@@ -1,6 +1,6 @@
 # Autenticação de usuário
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # Aqui serão definidas as rotas de autenticação (URLs) e telas correspondentes
 
@@ -9,14 +9,16 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html", text="Texto qualquer")
+    # Jinga permite a passagem de variáveis/valores para os templates, apenas referenciando-as na chamada
+    # No caso acima, a variável 'text' pode ser acessada no arquivo 'login.html'
 
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
-
+render_template()
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
