43f51f53c6fbe95a7b99d3f9e35bcddaa89e43ac
Lucas2 <contatolucasoficial0@gmail.com>
2023-04-12 20:21:43 -0300

    Commit 6 - estrutura if (templates)

--- Website/auth.py
+++ Website/auth.py
@@ -9,15 +9,13 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template("login.html", text="Texto qualquer")
-    # Jinga permite a passagem de variáveis/valores para os templates, apenas referenciando-as na chamada
-    # No caso acima, a variável 'text' pode ser acessada no arquivo 'login.html'
+    return render_template("login.html", boolean=True)
 
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
-render_template()
+
 
 @auth.route('/sign-up')
 def sign_up():
